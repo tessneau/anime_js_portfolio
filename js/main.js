cbeada6b5a80458ceef72a59d11b743929c98636
Tess Neau <tessneau@gmail.com>
2019-09-24 14:51:19 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const panels = document.querySelectorAll('.panels div')
+const main = document.querySelector('.main-container')
+const content = document.querySelector('.content-container')
+const links = document.querySelector('.links')
+
+function navHandler(e) {
+  if (e.target.className === "links--about") {
+    console.log('yo')
+    anime({
+      targets: content,
+      backgroundColor: 'red',
+      opacity: 1,
+      easing: 'easeOutExpo',
+      duration: 3000
+      })
+  }
+}
+
+links.addEventListener('click', navHandler)
+
+let tl = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 750
+});
+
+tl.add({
+  targets: panels,
+  backgroundColor: 'rgb(237, 202, 165)',
+  width: '100%',
+  delay: anime.stagger(100)
+})
+tl.add({
+  targets: main,
+  top: '0',
+  opacity: 1,
+  duration: 4000
+})
